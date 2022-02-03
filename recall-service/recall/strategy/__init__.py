c5e19f368eb70ef21ded2c22eb0b8b531b28c037
huichuan <lhc14124908@163.com>
2022-02-03 13:31:40 +0800

    add

--- recall-service/recall/strategy/__init__.py
+++ recall-service/recall/strategy/__init__.py
@@ -3,11 +3,13 @@ from recall.strategy.simple_strategy import SimpleRecallStrategy
 from recall.strategy.high_rating_strategy import HighRatingStrategy
 from recall.strategy.most_rating_strategy import MostRatingStrategy
 from recall.strategy.similar_anime_strategy import SimilarAnimeStrategy
+from recall.strategy.user_embedding_strategy import UserEmbeddingStrategy
 
 __all__ = [
     RecallStrategy,
     SimpleRecallStrategy,
     HighRatingStrategy,
     MostRatingStrategy,
-    SimilarAnimeStrategy
+    SimilarAnimeStrategy,
+    UserEmbeddingStrategy
 ]
