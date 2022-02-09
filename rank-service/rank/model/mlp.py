9e9cf39ee55e9de0ff29dc1e6e8c5aeecc30cf45
huichuan <lhc14124908@163.com>
2022-02-10 00:04:28 +0800

    add

---
+++ rank-service/rank/model/mlp.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# @Time : 2022/2/10 12:01 上午
+# @Author : huichuan LI
+# @File : mlp.py
+# @Software: PyCharm
+from tensorflow import keras
+
+
+class RankModel:
+    def __init__(self) -> None:
+        self.model = keras.models.load_model('mlp_model')
+
+    ## xs: list of features
+    ## [
+    ##   [A],
+    ##   [B],
+    ##   [C],
+    ## ]
+    ##
+    ## i.e.
+    ## [
+    ##   [[A cate features1], [A cate features2], [A num features]],
+    ##   [[B cate features1], [B cate features2], [B num features]],
+    ##   [[C cate features1], [C cate features2], [C num features]],
+    ## ]
+    def predict(self, xs):
+        return self.model.predict(xs)
